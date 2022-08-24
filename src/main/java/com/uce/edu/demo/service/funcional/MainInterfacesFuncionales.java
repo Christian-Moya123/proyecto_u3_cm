c5f2c8b16f28e89a0b7ed0d8f62bd7d2f650f4bf
Christian Moya <cmoyacharro@gamil.com>
2022-08-23 20:53:08 -0500

    JTALLER 34: Interfaces Funcionales y Métodos High Order

--- src/main/java/com/uce/edu/demo/service/funcional/MainInterfacesFuncionales.java
+++ src/main/java/com/uce/edu/demo/service/funcional/MainInterfacesFuncionales.java
@@ -10,6 +10,11 @@ public class MainInterfacesFuncionales {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ConsumoMetodoHighOrder consumo = new ConsumoMetodoHighOrder();
+		
+	
+		
+		
 		//SUPPLIER
 		//clases
 		
@@ -18,6 +23,18 @@ public class MainInterfacesFuncionales {
 		
 		IPersonaSupplier<String> supplierTE =  new PersonaSupplierImpl();
 		logger.info("Supllier Clase 2 : " + supplierTE.getNombre());
+		
+		String valorHO = consumo.consumirSupplier(() -> "Hola mundo");
+		logger.info("HO supplier :  " + valorHO);	
+				
+		String valorHO1 = consumo.consumirSupplier(() -> {
+					String valorConsultado = "17200";
+					return valorConsultado;}
+				);
+		logger.info("HO1 supplier :  " + valorHO1);
+		
+		//JAVA
+		
 		
 		//lambdas
 		IPersonaSupplier<String> supplierLambda = () -> "Christian 2";
@@ -32,26 +49,63 @@ public class MainInterfacesFuncionales {
 		//clases
 		IPersonaConsumer<String> consumerClase = new PersonaConsumerImpl();
 		consumerClase.accept("Prueba consumo");
+		//metodos high order
+
+		consumo.consumirConsumer(valor -> System.out.println(valor), 2);
 		
 		//lambdas
 		IPersonaConsumer<String> consumerLambda = cadena -> System.out.println(cadena);
 		consumerLambda.accept("Prueba consumo Lambda");
+		
 		//PREDICATE
 		//clases
+		
 		//lambdas
-				
+		IPersonaPredicate<String> predicateLambda = cadena -> cadena.contains("Z");
+		
+		logger.info("Predicate Lambda :  " + predicateLambda.evaluar("Chistian"));	
+		
+		boolean respuesta = consumo.consumirPredicate(cadena -> cadena.contains("Z"), "Edizon");
+		logger.info("HO1 consumer :  " + respuesta);
+		
+		String valorFinalHO = consumo.consumirFunction(valor -> {
+			String retorno = valor.toString()+"A";
+			return retorno;
+			
+		}, 1);
+		logger.info("HO1 functionr :  " + valorFinalHO);
 		
 		
 		//FUNCTION
 		//clases
 		//lambdas
-				
+		IPersonaFunction<Integer, String> functionLambda = cadena -> { 
+			Integer valor = Integer.parseInt(cadena);
+			Integer valorFinal = valor-2;
+			return valorFinal;
+		};
+		logger.info("Function Lambda :  " + functionLambda.aplicar("7"));	
+		
 		
 		//UNARYOPERATOR
 		//clases
 		//lambdas
+		IPersonaUnaryOperator<String> unaryLambda = cadena ->{
+			String valorFinal = cadena.concat("sufijo");
+			return valorFinal;
+		};
 				
-
+		unaryLambda.apply("Daniel");
+		
+		
+		
+	
+		
+		
+		
+		
+	
 	}
+	
 
 }
